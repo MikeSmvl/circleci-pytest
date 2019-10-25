60ef5546b9e12bcbfcd6d8d0d4bb0762b34166d8
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-10-25 00:54:33 -0400

    Fix dependency issue

--- sample_test.py
+++ sample_test.py
@@ -1,5 +1,15 @@
+import csv
+import urllib.request
+import codecs
+
+url = 'https://raw.githubusercontent.com/MikeSmvl/circleci-pytest/master/sample.csv'
+ftpstream = urllib.request.urlopen(url)
+csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
+
 def inc(x):
     return x + 1
 
 def test_answer():
-    assert inc(4) == 5
+  for line in csvfile:
+    print(line)
+  assert inc(4) == 5
